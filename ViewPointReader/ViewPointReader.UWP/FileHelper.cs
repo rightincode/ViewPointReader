cabd603c1dbfdd3bd793a930b81b67fb2b886691
Richard L. Taylor Jr <rtaylor@sentryone.com>
2019-05-14 01:17:49 -0400

    Added UWP support/project

---
+++ ViewPointReader/ViewPointReader.UWP/FileHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using ViewPointReader.UWP;
+using Windows.Storage;
+using ViewPointReader.Data.Interfaces;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace ViewPointReader.UWP
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            return Path.Combine(ApplicationData.Current.LocalFolder.Path, filename);
+        }
+    }
+}

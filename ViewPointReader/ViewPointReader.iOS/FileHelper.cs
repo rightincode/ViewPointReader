6a97598bc49bcafba863966c42eb5b662c0df594
Richard L. Taylor Jr <rtaylor@sentryone.com>
2019-05-14 00:00:44 -0400

    Updates for iOS

---
+++ ViewPointReader/ViewPointReader.iOS/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using ViewPointReader.Data.Interfaces;
+using ViewPointReader.iOS;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace ViewPointReader.iOS
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string libFolder = Path.Combine(docFolder, "..", "Library", "Databases");
+
+            if (!Directory.Exists(libFolder))
+            {
+                Directory.CreateDirectory(libFolder);
+            }
+
+            return Path.Combine(libFolder, filename);
+        }
+    }
+}
